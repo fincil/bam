b4817613431b05e95670bb71f0c4bae6991847dd
wooklym <wooklym@gmail.com>
2016-06-07 19:56:34 +0900

    first commit

---
+++ bam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bam
+//
+//  Created by HSWook on 2016. 6. 7..
+//  Copyright © 2016년 fincil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
